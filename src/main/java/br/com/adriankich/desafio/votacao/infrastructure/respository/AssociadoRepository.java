e0de181da6c9d5c268db984054fb6e3ecb591356
Adrian Kich <kich.adrian@gmail.com>
2024-10-13 11:59:59 -0300

    feat: add new method in AssociadoRepository

--- src/main/java/br/com/adriankich/desafio/votacao/infrastructure/respository/AssociadoRepository.java
+++ src/main/java/br/com/adriankich/desafio/votacao/infrastructure/respository/AssociadoRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 @Repository
 public interface AssociadoRepository extends JpaRepository<Associado, Long> {
     Optional<Associado> findByDocument(String document);
+    boolean existsByDocument(String document);
 }
